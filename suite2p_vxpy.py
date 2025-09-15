3a8f2da72f3db0931de17b8821c9a8521b5da9a5
Tim <tim.hladnik@gmail.com>
2025-09-15 17:28:32 +0200

    Works, incl. first collection queries; but slow selects for mysql backend (possibly UUIDs);

--- suite2p_vxpy.py
+++ suite2p_vxpy.py
@@ -1,5 +1,8 @@
 from cmath import phase
 
+import numpy as np
+import tqdm
+
 from entarchy import Entarchy, Entity, Collection
 
 __all__ = ['Animal', 'Recording', 'Roi', 'Phase', 'Suite2PVxPy']
@@ -41,44 +44,86 @@ class Suite2PVxPy(Entarchy):
 
         import random
 
-        for i in range(3):
-            animal = Animal(self, _id=f'Animal_{i}', _parent=None)
-            self.add_new_entity(animal)
-            for j in range(7):
-                recording = Recording(self, _id=f'Recording_{j}', _parent=animal)
-                self.add_new_entity(recording)
-                for p in range(300):
-                    phase = Phase(self, _id=f'Phase_{p}', _parent=recording)
-                    self.add_new_entity(phase)
+        # Use in context to control commits and speed up adding multiple entities and their attributes
+        with self:
 
-                for r in range(random.randint(400, 800)):
-                    roi = Roi(self, _id=f'Roi_{r}', _parent=recording)
-                    self.add_new_entity(roi)
+            for i in tqdm.tqdm(range(3), desc='Animals', position=0):
+                animal = Animal(self, _id=f'Animal_{i}', _parent=None)
+                for k, v in {'age': random.randint(24, 96),
+                             'weight': random.randint(20, 100) / 10,
+                             'strain': random.choice(['jf1', 'mpn400', 'jf7'])}.items():
+                    animal[k] = v
+                self.add_new_entity(animal)
 
-            self.commit()
+                for j in tqdm.tqdm(range(7), desc='Recordings', position=1):
+                    recording = Recording(self, _id=f'Recording_{j}', _parent=animal)
+                    self.add_new_entity(recording)
+
+                    for jj in range(5):
+                        recording[f'param_int_{jj}'] = random.randint(0, 1000)
+                    for jj in range(5):
+                        recording[f'param_float_{jj}'] = random.randint(0, 10000) / 10
+                    for jj in range(5):
+                        recording[f'param_string_{jj}'] = random.choice(['foo', 'bar', 'baz', 'lorem', 'ipsum', 'dolor'])
+                    for jj in range(5):
+                        recording[f'param_array_{jj}'] = np.random.rand(random.randint(10, 100))
+                    for jj in range(5):
+                        recording[f'param_list_{jj}'] = [random.randint(0, 100) for _ in range(random.randint(10, 100))]
+
+                    for p in tqdm.tqdm(range(300), desc='Phases', position=2):
+                        phase = Phase(self, _id=f'Phase_{p}', _parent=recording)
+                        self.add_new_entity(phase)
+
+                    for r in tqdm.tqdm(range(random.randint(400, 800)), desc='Rois', position=2):
+                        roi = Roi(self, _id=f'Roi_{r}', _parent=recording)
+                        self.add_new_entity(roi)
+
+                        for jj in range(5):
+                            roi[f'param_int_{jj}'] = random.randint(0, 1000)
+                        for jj in range(5):
+                            roi[f'param_float_{jj}'] = random.randint(0, 10000) / 10
+                        for jj in range(5):
+                            roi[f'param_string_{jj}'] = random.choice(['foo', 'bar', 'baz', 'lorem', 'ipsum', 'dolor'])
+                        for jj in range(5):
+                            roi[f'param_array_{jj}'] = np.random.rand(random.randint(10, 100))
+                        for jj in range(5):
+                            roi[f'param_list_{jj}'] = [random.randint(0, 100) for _ in range(random.randint(10, 100))]
+
+                    # Commit after each recording
+                    self.commit()
 
 
 if __name__ == '__main__':
 
-    _backend = MySQLBackend(dbhost='172.25.240.200',
-                            dbname=f'agarrenberg_test123',
-                            dbuser='thladnik',
-                            dbpassword=open('pw.txt', 'r').readline().strip(),)
+    # Old
+    # '172.25.240.200'
+    # agarrenberg_
+    # thladnik
 
-    # try:
-    #     my_entarchy = Suite2PVxPy('./my_test/path/')
-    #     my_entarchy.delete()
-    # except:
-    #     pass
-    #
-    # my_entarchy = Suite2PVxPy.create('./my_test/path/', _backend)
-    # my_entarchy.digest('')
+    recreate = False
 
-    my_entarchy = Suite2PVxPy('./my_test/path/')
+    # Recreate entarchy
+    if recreate:
+        try:
+            ente = Suite2PVxPy('./dev_test/path/')
+            ente.delete()
+        except:
+            pass
 
+        # Get connection data
+        with open('secret.txt', 'r') as f:
+            mysqldata = [f.readline().strip() for _ in range(4)]
 
-    recs = my_entarchy.get(Recording)
-    rec = recs[0]
+        _backend = MySQLBackend(dbhost=mysqldata[0],
+                                dbname=f'{mysqldata[1]}_test123',
+                                dbuser=mysqldata[2],
+                                dbpassword=mysqldata[3])
 
-    print('Hello!!')
+        ente = Suite2PVxPy.create('./dev_test/path/', _backend)
+        ente.digest('')
+
+    ente = Suite2PVxPy('./dev_test/path/', debug=True)
+
+    recs = ente.get(Recording, 'param_int_1 > 10')
+
     print('')
