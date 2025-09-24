c25b628701aacd2109521809d93259589ac6d018
thladnik <tim.github@hladnik.de>
2025-09-24 16:42:02 +0200

    Fix commit of entities (conditions and copy _entities_to_update); add alive_bar for the fun; fix __prev_analysis for contexts;

--- suite2p_vxpy.py
+++ suite2p_vxpy.py
@@ -44,19 +44,25 @@ class Suite2PVxPy(Entarchy):
 
         import random
 
-        # Use in context to control commits and speed up adding multiple entities and their attributes
+        # Use in context to control commits
+        #  and speed up adding multiple entities and their attributes
         with self:
+            R = 0
 
-            for i in tqdm.tqdm(range(3), desc='Animals', position=0):
+            # for i in tqdm.tqdm(range(3), desc='Animals', position=0):
+            for i in range(3):
                 animal = Animal(self, _id=f'Animal_{i}', _parent=None)
+                print(f'Add {animal}')
                 for k, v in {'age': random.randint(24, 96),
                              'weight': random.randint(20, 100) / 10,
                              'strain': random.choice(['jf1', 'mpn400', 'jf7'])}.items():
                     animal[k] = v
                 self.add_new_entity(animal)
 
-                for j in tqdm.tqdm(range(7), desc='Recordings', position=1, leave=False):
+                # for j in tqdm.tqdm(range(7), desc='Recordings', position=1, leave=False):
+                for j in range(random.randint(3, 8)):
                     recording = Recording(self, _id=f'Recording_{j}', _parent=animal)
+                    print(f'> Add {recording}')
                     self.add_new_entity(recording)
 
                     for jj in range(5):
@@ -70,16 +76,22 @@ class Suite2PVxPy(Entarchy):
                     for jj in range(5):
                         recording[f'param_list_{jj}'] = [random.randint(0, 100) for _ in range(random.randint(10, 100))]
 
-                    for p in tqdm.tqdm(range(300), desc='Phases', position=2, leave=False):
+                    # for p in tqdm.tqdm(range(300), desc='Phases', position=2, leave=False):
+                    p_num = 300
+                    print(f'>> Add {p_num} Phases')
+                    for p in range(p_num):
                         phase = Phase(self, _id=f'Phase_{p}', _parent=recording)
                         self.add_new_entity(phase)
 
-                    for r in tqdm.tqdm(range(random.randint(400, 800)), desc='Rois', position=2, leave=False):
+                    # for r in tqdm.tqdm(range(random.randint(400, 800)), desc='Rois', position=2, leave=False):
+                    r_num = random.randint(400, 800)
+                    print(f'>> Add {r_num} Rois')
+                    for r in range(r_num):
                         roi = Roi(self, _id=f'Roi_{r}', _parent=recording)
                         self.add_new_entity(roi)
 
                         for jj in range(5):
-                            roi[f'param_int_{jj}'] = random.randint(0, 1000)
+                            roi[f'param_int_{jj}'] = random.randint(1, 999)
                         for jj in range(5):
                             roi[f'param_float_{jj}'] = random.randint(0, 10000) / 10
                         for jj in range(5):
@@ -88,23 +100,15 @@ class Suite2PVxPy(Entarchy):
                             roi[f'param_array_{jj}'] = np.random.rand(random.randint(10, 100))
                         for jj in range(5):
                             roi[f'param_list_{jj}'] = [random.randint(0, 100) for _ in range(random.randint(10, 100))]
-                        # Commit after each ROI
-                        self.commit()
 
                     # Commit after each recording
                     self.commit()
-                # Commit after each animal
-                self.commit()
+
 
 
 if __name__ == '__main__':
 
-    # Old
-    # '172.25.240.200'
-    # agarrenberg_
-    # thladnik
-
-    recreate = False
+    recreate = True
     if 'recreate' in sys.argv:
         recreate = True
 
@@ -127,14 +131,3 @@ if __name__ == '__main__':
 
         ente = Suite2PVxPy.create('./dev_test/path/', _backend)
         ente.digest('')
-
-    ente = Suite2PVxPy('./dev_test/path/', debug=False)
-
-    recs = ente.get(Recording, 'param_int_1 > 10')
-
-    rois = ente.get(Roi)
-    rois1 = ente.get(Roi, 'param_int_1 >= 200')
-    rois2 = ente.get(Roi, 'param_int_1 <= 800')
-    rois3 = ente.get(Roi, 'param_int_1 >= 950')
-
-    print('')
